be68613207b483b6553e56c2aff79deb7427ac31
Faxriddin Xushnazarov <83649406+XushnazarovFaxriddin@users.noreply.github.com>
2022-11-30 22:34:01 +0500

    Add project files.

---
+++ TodoMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Todo()
+        {
+            int todoCount = 154;
+            return View(todoCount);
+        }
+    }
+}
---
+++ TodoMvcApp/wwwroot/js/main.js
@@ -0,0 +1,1 @@
+﻿console.log("main.js file ishga tushdi!")
